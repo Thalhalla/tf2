f4e9290134922a552a443f0bec38e194dff85132
Josh Cox <josh@webhosting.coop>
2016-12-29 10:58:10 -0600

    errantline

--- assets/start.sh
+++ assets/start.sh
@@ -15,7 +15,6 @@ sed -i "s/REPLACEME_TF2_MAIL/$TF2_MAIL/" server.cfg
 sed -i "s/REPLACEME_TF2_EXEC/$TF2_EXEC/" server.cfg
 # server.cfg
 [ -f /home/steam/serverfiles/tf2/tf/cfg/server.cfg ] && echo 'server.cfg exists already' || cp -v /assets/server.cfg /home/steam/serverfiles/tf2/tf/cfg/
-cp /assets/server.cfg /home/steam/serverfiles/tf2/tf/cfg/
 
 # map cycle 
 [ -f /home/steam/serverfiles/tf2/tf/cfg/mapcycle.txt ] && echo 'mapcycle.txt exists already' || cp -v /assets/mapcycle.txt /home/steam/serverfiles/tf2/tf/cfg/
