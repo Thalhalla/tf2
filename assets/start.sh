bd697a53eb972a7bfc0051b6b0f8056a9591152f
Josh Cox <josh@webhosting.coop>
2016-12-29 10:43:44 -0600

    Only cp if they don't exist

--- assets/start.sh
+++ assets/start.sh
@@ -8,16 +8,17 @@ sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
 bash /home/steam/steamcmd.sh +runscript /home/steam/steamer.txt
 
-# server.cfg
-cp /assets/server.cfg /home/steam/serverfiles/tf2/tf/cfg/
-cd /home/steam/serverfiles/tf2/tf/cfg/
+cd /assets/
 sed -i "s/REPLACEME_TF2_HOSTNAME/$TF2_HOSTNAME/" server.cfg
 sed -i "s/REPLACEME_TF2_PASSWORD/$TF2_PASSWORD/" server.cfg
 sed -i "s/REPLACEME_TF2_MAIL/$TF2_MAIL/" server.cfg
 sed -i "s/REPLACEME_TF2_EXEC/$TF2_EXEC/" server.cfg
+# server.cfg
+[ -f /home/steam/serverfiles/tf2/tf/cfg/server.cfg ] && echo 'server.cfg exists already' || cp -v /assets/server.cfg /home/steam/serverfiles/tf2/tf/cfg/
+cp /assets/server.cfg /home/steam/serverfiles/tf2/tf/cfg/
 
 # map cycle 
-cp /assets/mapcycle.txt /home/steam/serverfiles/tf2/tf/cfg/
+[ -f /home/steam/serverfiles/tf2/tf/cfg/mapcycle.txt ] && echo 'mapcycle.txt exists already' || cp -v /assets/mapcycle.txt /home/steam/serverfiles/tf2/tf/cfg/
 
 # Run
 bash /assets/run.sh
