98fde60de193608be3c4b1d3244ab5fbe4159931
Josh Cox <josh@webhosting.coop>
2016-12-29 14:15:45 -0600

    noip

--- assets/run.sh
+++ assets/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /home/steam/serverfiles/tf2
-./srcds_run -console -game tf -nohltv +sv_pure 1 +map ctf_2fort +maxplayers 24 +ip $IP +sv_setsteamaccount $STEAM_GLST
+# ./srcds_run -console -game tf -nohltv +sv_pure 1 +map ctf_2fort +maxplayers 24 +ip $IP +sv_setsteamaccount $STEAM_GLST
+./srcds_run -console -game tf -nohltv +sv_pure 1 +map ctf_2fort +maxplayers 24 +sv_setsteamaccount $STEAM_GLST
